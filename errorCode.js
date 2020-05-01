b82086fd3d39bf79fb565e1df384a2a9286f12d0
kamil.danielski@gmail.com <kamil.danielski@gmail.com>
2020-05-01 21:59:29 +0200

    Adding register and signin route

---
+++ errorCode.js
@@ -0,0 +1,10 @@
+const error = {
+    23505: {
+        details: 'Podany email już został użyty.'
+    }
+}
+
+
+module.exports = {
+    error
+}
--- server.js
+++ server.js
@@ -1,10 +1,77 @@
 const express = require('express');
+const knex = require('knex');
+const bcrypt = require('bcrypt');
+const bodyParser = require('body-parser');
+const { error } = require('./errorCode')
+
+const db = knex({
+    client: 'pg',
+    connection: {
+      host : '127.0.0.1',
+      user : 'postgres',
+      password : 'Test123',
+      database : 'BookRegister'
+    }
+  });
+
 
 const app = express();
+app.use(bodyParser.json());
+
 
 app.get('/', (req,res) => {
-    res.send("it's working!");
+    db.select('*').from('User')
+    .then(user => {res.json(user)})
+    ;
 })
+
+app.post('/register', (req, res) => {
+    const { name, email, createDate = new Date(), password} = req.body;
+    const salt = bcrypt.genSaltSync(10);
+    const hash = bcrypt.hashSync(password, salt);
+
+    db.transaction(trx => {
+        trx
+        .insert({
+            name: name,
+            email: email,
+            create_date: createDate
+        })
+        .into('user')
+        .returning('id')
+        .then(userId => {
+            return trx
+                .insert({
+                    user_id: Number(userId),
+                    password: hash
+                })
+                .into('login')
+                .returning('*')
+                .then(() => res.json(userId))
+                .catch(err => console.log(err))
+                
+        })
+        
+        .then(trx.commit)
+        .catch(trx.rollback)
+    })
+    .catch(err => {
+            res.json(err)
+    });
+
+})
+
+app.post('/signIn', (req,res) => {
+    const {password, email} = req.body;
+    db.select('user.email', 'login.password')
+        .from('user')
+        .innerJoin('login','user.id','login.user_id')
+        .then(data => {
+            res.send(data)
+        })   
+        .catch(err =>  res.send(err)); 
+})
+
 
 process.env.PORT = 8080;
 
